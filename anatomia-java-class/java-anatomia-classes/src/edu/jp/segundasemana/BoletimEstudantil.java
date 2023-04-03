1186c1476e420831c06febc4915b7b88b0b2ca70
João Pedro C. de Albuquerque Silva <joaopedro.cavalcante534@gmail.com>
2023-04-03 11:18:54 -0300

    novo repositório para projetos

---
+++ anatomia-java-class/java-anatomia-classes/src/edu/jp/segundasemana/BoletimEstudantil.java
@@ -0,0 +1,14 @@
+package edu.jp.segundasemana;
+public class BoletimEstudantil {
+
+    public static void  main (String [] args){
+        int mediaFinal = 6;
+        if (mediaFinal < 6)
+            System.out.println("Reprovado");
+        else if (mediaFinal == 6)
+            System.out.println ("Prova de Minerva");
+        else    
+            System.out.println ("Aprovado");
+    }
+    
+}
---
+++ usando-IDE-Eclipse/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,20 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato(n);		
+		Livros livros = new Livros ();
+		
+	}
+
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+	
+}
